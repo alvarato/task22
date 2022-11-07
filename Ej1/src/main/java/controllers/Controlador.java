600b0799b5de0c578bf1975bd3cf1fcc57b80346
Oscar Vilamitjana <oscar08850>
2022-11-07 10:04:08 +0100

    POM update y Cliente.java creado

---
+++ Ej1/src/main/java/controllers/Controlador.java
@@ -0,0 +1,11 @@
+package controllers;
+
+public class Controlador {
+	
+	
+	public void insertInto(String nombre, ) {
+		
+	}
+	// INSERT INTO cliente(nombre, apellido, direccion, dni, fecha) VALUES ()
+
+}
