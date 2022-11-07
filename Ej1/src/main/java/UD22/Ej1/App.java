98501d21754401859b5997abf535dc2fd4c89e48
Chals <csanchezbasket@gmail.com>
2022-11-07 09:12:39 +0100

    Commit inicial

---
+++ Ej1/src/main/java/UD22/Ej1/App.java
@@ -0,0 +1,13 @@
+package UD22.Ej1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
